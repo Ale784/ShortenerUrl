9ffd893b36922d0797945e1246f92d53d1b50df3
ale784 <acorrea7412@gmail.com>
2023-12-29 09:58:02 -0500

    style: add tailwind and refactor general design

---
+++ web/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      one: ["One", "sans-serif"]
+    },
+    extend: {},
+  },
+  plugins: [],
+}
